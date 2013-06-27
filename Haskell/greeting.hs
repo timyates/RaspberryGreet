11447c994afe4fc02207c2b179a8d7229fc2ce50
Tim Yates <tyates@picr.man.ac.uk>
2013-06-27 20:11:13 +0100

    Added Haskell comments

--- Haskell/greeting.hs
+++ Haskell/greeting.hs
@@ -1,6 +1,11 @@
 import System.IO
 main = do
+    -- Print our question
     putStr "Enter your name: "
     hFlush stdout
+
+    -- Then read the response from the user into `name`
     name <- getLine
+    
+    -- And print our greeting
     putStrLn ( "Hello " ++ name )
